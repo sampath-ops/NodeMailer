58eda4b67fd5f47635ac099ba1af055f5529aede
samapth-ops <sampathkaali002@gmail.com>
2022-03-20 12:22:03 +0530

    cors used

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const route = express.Router();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://gcesynergy2022.netlify.app/'
+}))
 
 app.use('/v1', route);
 
