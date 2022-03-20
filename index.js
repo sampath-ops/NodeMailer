2457bb0d0be0fc98a7c9d0a7e8b273dd08c7f2d1
samapth-ops <sampathkaali002@gmail.com>
2022-03-20 13:40:44 +0530

    gmail account changed

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'gcesynergy2022@gmail.com',
-        pass: 'Thiru@20012',
+        user: 'sampathkaali002@gmail.com',
+        pass: 'sam_sri002',
     },
     tls: {
         ciphers:'SSLv3',
